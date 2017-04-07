dcd9591db96b5936e189d0d37ec08482fa63d5ed
Morgan <quixoticbibliomania@gmail.com>
2017-04-07 09:42:07 -0400

    basic setting port up

--- Connection-Manager-Server/src/server/ConnectionServer.java
+++ Connection-Manager-Server/src/server/ConnectionServer.java
@@ -1,7 +1,19 @@
 package server;
 
+import java.io.IOException;
+import java.net.*;
+/**
+ * The Server used by the messaging application
+ * @author Morgan
+ *
+ */
 public class ConnectionServer {
+	public static final int PORTNUM = 3576;
 	public static void main(String[] args) {
-		
+		try (ServerSocket connection = new ServerSocket(PORTNUM)) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
